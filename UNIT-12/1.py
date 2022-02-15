bccb02d671cf6fd5cc4d912fdfcedacb81c7074b
stgim <stgim@mobigen.com>
2022-02-15 18:09:22 +0900

    신규 수업 추가

---
+++ UNIT-12/1.py
@@ -0,0 +1,13 @@
+# 16.1 while 반복문 사용하기
+# 100번 출력하기
+i = 0
+while i < 100:
+    print('Hello, world!')
+    i += 1
+
+# 16-2 초깃값을 1부터 시작하기
+# i를 입력하여 100번 출력하기
+i = 1
+while i < 100:
+    print('Hello, world!', i)
+    i += 1
