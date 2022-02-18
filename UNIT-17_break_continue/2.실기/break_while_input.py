b38bdb399c8155a5a7ca13993b2571904c1aa716
stgim <stgim@mobigen.com>
2022-02-18 18:20:26 +0900

    업데이트

---
+++ UNIT-17_break_continue/2.실기/break_while_input.py
@@ -0,0 +1,21 @@
+
+
+count = int(input('반복할 횟수를 입력하세요 : '))
+
+i = 1
+while True:
+    print(i)
+    if i == count:
+        break
+    i += 1
+
+########################################
+
+# range() 사용하여 반복 횟수 지정하기
+
+i = 1
+while range(0, 100): 
+    print(i)
+    if i == count:
+        break
+    i += 1
